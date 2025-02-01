b8d820cd31a59d8ec62416a62158ba3d2086200e
Jose P. Leitao <jose.p.leitao@gmail.com>
2025-02-01 14:57:08 -0300

    Update tasks.json

--- app/working/test_kaggle.py
+++ app/working/test_kaggle.py
@@ -7,15 +7,13 @@ import pandas as pd
 
 MAX_DS = 20
 MAX_PAGE = math.ceil(MAX_DS/20)
+KEYWORD = 'Computer Science'
 
 datasets = []
 for i in range(MAX_PAGE):
-    datasets = datasets + api.datasets_list(search='Computer Science', page=i)
+    datasets = datasets + api.datasets_list(search=KEYWORD, page=i)
                 
 df = pd.DataFrame(datasets)
 #df.to_csv('kaggle_ds_list.csv')
 print(df.head())
 
-# df = pd.DataFrame(datasets)
-# # df.to_csv('kaggle_ds_list.csv')
-# print(df.columns.values)
